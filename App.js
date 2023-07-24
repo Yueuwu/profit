71a1c49f3fd8931b3d77371d2f0a440ffc051b28
Yueuwu <vladdenega1@gmail.com>
2023-07-24 12:05:01 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import { StyleSheet, Text, TextInput, View } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [income, setIncome] = useState()
+
+  const onSetIncome = (value) => {
+    const isInputValid = isNaN(Number(value))
+    if (isInputValid){
+      return;
+    }
+    setIncome(value)
+  }
+
+	const date = new Date().toLocaleDateString()
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>
+				Сколько ты заработала сегодня ({date}) {income}?
+			</Text>
+			<TextInput
+				style={styles.input}
+        value={income}
+        onChangeText={value => onSetIncome(value)}
+				placeholder="Введи сумму..."
+			/>
+			<StatusBar backgroundColor="#CEBBF5" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	input: {
+		height: 40,
+		width: "70%",
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+})
